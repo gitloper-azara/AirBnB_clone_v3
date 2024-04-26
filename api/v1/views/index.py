293c48347dc929afdac7be6f42602c8ca294021a
Azara <yyushahu02@gmail.com>
2024-04-26 17:53:11 +0000

    add endpoint feature that retrieves number of each obj by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,13 @@ index module for API
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status')
@@ -14,3 +21,19 @@ def return_status():
         "status": "OK"
     }
     return jsonify(status)
+
+@app_views.route('/stats')
+def num_obj_by_type():
+    """returns the number of each obj by type"""
+    classes = [
+        "Amenity",
+        "City",
+        "Place",
+        "Review",
+        "State",
+        "User"]
+    json_return = {}
+    for classs in classes:
+        item_count = storage.count(eval(classs))
+        json_return[classs] = item_count
+    return jsonify(json_return)

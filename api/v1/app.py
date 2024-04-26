1f9183444af20c1b823b24690d0373ecd9f33ed3
Azara <yyushahu02@gmail.com>
2024-04-26 18:05:57 +0000

    handle 404 pages with custom message

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 starts API
 """
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -12,6 +12,15 @@ from os import getenv
 # create a variable app, instance of Flask
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """custom 404 display using json"""
+    err_message = {
+        "error": "Not found"
+    }
+    return jsonify(err_message), 404
 
 
 if __name__ == '__main__':

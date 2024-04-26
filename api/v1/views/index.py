e50fec3f96ab95fd2d4e51635babec1062b68165
Azara <yyushahu02@gmail.com>
2024-04-26 23:22:09 +0000

    import required modules

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,6 +6,12 @@ index module for API
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status')
